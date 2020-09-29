a478331a9f03e128b04a0bd2f67c95832494bf7c
Gabriel Wassan <gabrielwassan@gmail.com>
2020-09-28 23:45:11 -0300

    feat(challenge) - Added controller and service on appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlertController } from './alert/alert.controller';
+import { AlertService } from './alert/alert.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AlertController],
+  providers: [AppService, AlertService],
 })
 export class AppModule {}
