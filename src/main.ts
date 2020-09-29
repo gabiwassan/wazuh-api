15222e09c76a8d8a341775b80360c896da426d05
Gabriel Wassan <gabrielwassan@gmail.com>
2020-09-28 23:44:45 -0300

    feat(challenge) - Installed swagger for api documentation & config.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('Wazuh API')
+      .setDescription('Wazuh API')
+      .build(),
+  );
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
