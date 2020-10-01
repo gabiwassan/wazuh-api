2467079d0520b2b42a78d19d87cdf58e6ce1c8f4
Gabriel Wassan <gabrielwassan@gmal.com>
2020-09-30 21:04:01 -0300

    feat(challenge) - Create service and controller for Rule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { AlertController } from './alert/alert.controller';
 import { AlertService } from './alert/alert.service';
 import { AgentController } from './agent/agent.controller';
 import { AgentService } from './agent/agent.service';
+import { RuleController } from './rule/rule.controller';
+import { RuleService } from './rule/rule.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, AlertController, AgentController],
-  providers: [AppService, AlertService, AgentService],
+  controllers: [AppController, AlertController, AgentController, RuleController],
+  providers: [AppService, AlertService, AgentService, RuleService],
 })
 export class AppModule {}
