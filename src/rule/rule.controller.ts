a63c2d3e9c137c595666225ed28ca4160ae7cb70
Gabriel Wassan <gabrielwassan@gmal.com>
2020-09-30 21:25:13 -0300

    feat(challenge) - Added endpoint to take Rules and service to filter data.

--- src/rule/rule.controller.ts
+++ src/rule/rule.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Rule } from '../interface/alert/alert';
+import { ApiQuery, ApiTags } from '@nestjs/swagger';
+import { RuleService } from './rule.service';
 
+@ApiTags('Rules')
 @Controller('rule')
-export class RuleController {}
+export class RuleController {
+  constructor(private readonly ruleService: RuleService) {}
+
+  @Get('/filter')
+  @ApiQuery({ name: 'offset', type: Number })
+  @ApiQuery({ name: 'limit', type: Number })
+  filterRules(
+    @Query('offset') offset: number,
+    @Query('limit') limit: number,
+  ): Array<Rule> {
+    return this.ruleService.getBy(offset, limit);
+  }
+
+  @Get()
+  findAll(): Array<Rule> {
+    return this.ruleService.getAll();
+  }
+
+  @Get(':id')
+  @ApiQuery({ name: 'id', type: Number })
+  findOne(@Query('id') id: number): Rule {
+    return this.ruleService.findOne(id);
+  }
+}
--- src/rule/rule.service.ts
+++ src/rule/rule.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { Rule } from '../interface/alert/alert';
+import { reduceCollection } from '../utils/utils';
+import jsonData = require('../mocks/alerts/alerts.json');
 
 @Injectable()
-export class RuleService {}
+export class RuleService {
+  private getRules() {
+    return jsonData.map(rule => rule._source.rule as Rule);
+  }
+
+  private getRuleById(id: number) {
+    const rules = this.getRules();
+    // @ts-ignore
+    return rules.find(rules => rules.id === id);
+  }
+
+  getAll(): Array<Rule> {
+    return this.getRules();
+  }
+
+  findOne(id: number): any {
+    return this.getRuleById(id);
+  }
+
+  getBy(offset: number, limit: number): any {
+    const ruleList = this.getRules();
+    const dataRemoved = this.removeDuplicates(ruleList);
+
+    const dataReduced = reduceCollection(dataRemoved, offset, limit);
+
+    const result = this.findCountAlertByAgent(jsonData, dataReduced);
+
+    return { total_items: dataReduced.length, data: result };
+  }
+
+  private findCountAlertByAgent = (alerts, rules) => {
+    alerts.map(alert =>
+      rules.map(rule =>
+        alert._source.rule.id === rule.id
+          ? (rule.total_alerts = rule.total_alerts ? rule.total_alerts + 1 : 1)
+          : null,
+      ),
+    );
+
+    return rules;
+  };
+
+  private removeDuplicates = data => {
+    return data.filter(
+      (rule, index, self) =>
+        index ===
+        self.findIndex(t => t.place === rule.place && t.id === rule.id),
+    );
+  };
+}
