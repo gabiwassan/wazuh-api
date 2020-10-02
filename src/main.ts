0c827b3719a7c400e264d6fe69e075909e04d57b
Gabriel Wassan <gabrielwassan@gmail.com>
2020-10-02 16:14:09 -0300

    fix(challenge) - Config cors in api.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: false,
+    logger: ['error', 'log', 'warn'],
+  });
+  app.enableCors();
 
   const document = SwaggerModule.createDocument(
     app,
